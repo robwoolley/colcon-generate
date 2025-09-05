8e793afce11084eacbaa0bb8fa60f6e27c9afdac
Rob Woolley <rob.woolley@windriver.com>
2025-09-05 19:33:31 +0000

    Add package selection to generate
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- colcon_generate/verb/generate.py
+++ colcon_generate/verb/generate.py
@@ -1,3 +1,11 @@
+# Copyright 2016-2018 Dirk Thomas
+# Copyright 2025 Wind River Systems, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from colcon_core.package_selection import add_arguments \
+    as add_packages_arguments
+from colcon_core.package_selection import get_package_descriptors
+from colcon_core.package_selection import select_package_decorators
 from colcon_core.plugin_system import satisfies_version
 from colcon_core.verb import VerbExtensionPoint
 
@@ -8,7 +16,35 @@ class GenerateVerb(VerbExtensionPoint):
         super().__init__()
         satisfies_version(VerbExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
 
+    def add_arguments(self, *, parser):  # noqa: D102
+        # only added so that package selection arguments can be used
+        # which use the build directory to store state information
+        parser.add_argument(
+            '--build-base',
+            default='build',
+            help='The base path for all build directories (default: build)')
+
+        add_packages_arguments(parser)
+
     def main(self, *, context):  # noqa: D102
-        print("Colcon generate")
+        args = context.args
+
+        descriptors = get_package_descriptors(args)
+
+        # always perform topological order for the select package extensions
+        decorators = topological_order_packages(
+            descriptors, recursive_categories=('run', ))
+
+        select_package_decorators(args, decorators)
+
+        lines = []
+        for decorator in decorators:
+            if not decorator.selected:
+                continue
+            pkg = decorator.descriptor
+            lines.append(pkg.name + '\t' + str(pkg.path) + '\t(%s)' % pkg.type)
+
+        for line in lines:
+            print(line)
 
         return 0
