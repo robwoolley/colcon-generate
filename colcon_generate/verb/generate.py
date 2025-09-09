246db35d358618775804b5a20cc9195c1e27ef8c
Rob Woolley <rob.woolley@windriver.com>
2025-09-09 16:52:38 -0400

    Create bitbake subverb for generate
    This is needed to provide opportunities to add generate build
    system files for other platforms.
    
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- colcon_generate/verb/generate.py
+++ colcon_generate/verb/generate.py
@@ -15,26 +15,23 @@
 
 from colcon_core.package_selection import add_arguments \
     as add_packages_arguments
-from colcon_core.package_selection import get_package_descriptors
-from colcon_core.package_selection import select_package_decorators
 from colcon_core.plugin_system import satisfies_version
-from colcon_core.topological_order import topological_order_packages
 from colcon_core.verb import VerbExtensionPoint
-from colcon_generate.PackageMetadata import PackageMetadata
-from colcon_generate.BitbakeRecipe import BitbakeRecipe
-
-import os
-
+from colcon_generate.subverb import get_subverb_extensions
+from colcon_core.command import add_subparsers
 
 class GenerateVerb(VerbExtensionPoint):
     """Generate buildsystem recipes"""
-    ros_package_manifest = 'package.xml'
 
     def __init__(self):  # noqa: D107
         super().__init__()
         satisfies_version(VerbExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+        self._subparser = None
 
     def add_arguments(self, *, parser):  # noqa: D102
+        # print usage if no subverb is given
+        self.__subparser = parser
+
         # only added so that package selection arguments can be used
         # which use the build directory to store state information
         parser.add_argument(
@@ -42,53 +39,13 @@ class GenerateVerb(VerbExtensionPoint):
             default='build',
             help='The base path for all build directories (default: build)')
 
+        subverb_extensions = get_subverb_extensions()
+        add_subparsers(parser, 'colcon generate', subverb_extensions,
+                       attribute='subverb_name')
+
         add_packages_arguments(parser)
 
     def main(self, *, context):  # noqa: D102
-        args = context.args
-
-        descriptors = get_package_descriptors(args)
-
-        # always perform topological order for the select package extensions
-        decorators = topological_order_packages(
-            descriptors, recursive_categories=('run', ))
-
-        select_package_decorators(args, decorators)
-
-        lines = []
-        for decorator in decorators:
-            if not decorator.selected:
-                continue
-            pkg = decorator.descriptor
-
-            lines.append(f"{pkg.name:<30}\t{str(pkg.path):<30}\t({pkg.type})")
-
-            self.path = os.path.abspath(
-                os.path.join(os.getcwd(), str(pkg.path)))
-
-            self.build_base = os.path.abspath(os.path.join(
-                os.getcwd(), args.build_base, pkg.name))
-
-            package_manifest_path = os.path.join(pkg.path, self.ros_package_manifest)
-            if os.path.exists(package_manifest_path):
-                lines.append(f"\t- ROS package manifest: {package_manifest_path}")
-                with open(package_manifest_path, 'r') as h:
-                    package_manifest = h.read()
-                    pkg_metadata = PackageMetadata(package_manifest, None)
-                    bitbake_recipe = BitbakeRecipe(pkg_metadata)
-
-                ros_bitbake_recipe = os.path.join(self.build_base, bitbake_recipe.bitbake_recipe_filename())
-                lines.append(f"\t- Bitbake recipe: {ros_bitbake_recipe}")
-
-                os.makedirs(self.build_base, exist_ok=True)
-
-                with open(ros_bitbake_recipe, 'w') as h:
-                    h.write(bitbake_recipe.get_recipe_text())
-            else:
-                lines.append(f"\t- No ROS package manifest found for {pkg.name}")
-                continue
-
-        for line in lines:
-            print(line)
-
-        return 0
+        if context.args.subverb_name is None:
+            self.__subparser.print_usage()
+            return "Error: No subverb provided"
